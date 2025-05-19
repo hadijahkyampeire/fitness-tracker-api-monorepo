6ba99a000a3f70d0096785bed3e7f026f9609d89
hadijahkyampeire <hadijah315@gmail.com>
2025-05-19 17:56:34 -0500

    add coach

--- user-service/src/main/resources/import.sql
+++ user-service/src/main/resources/import.sql
@@ -7,6 +7,8 @@ INSERT INTO role (id, name) VALUES (3, 'ADMIN');
 INSERT INTO users VALUES (DEFAULT, 1,'hadijah@example.com', '$2a$10$QM3vGBggMvygKHiBrBL39.Lf7owH61D/f7j2RBNsGWXILr0UO1DfO',  'Hadijah'); -- password: user123
 
 INSERT INTO users VALUES (DEFAULT, 2,'mariam@example.com', '$2a$10$P5rgd08r1WzhACRjgP.ZlujPlox4cGu3WxC/MCIL3ZtZ4xdz05ZGu', 'Mariam'); -- password: coach123
+INSERT INTO users VALUES (DEFAULT, 2,'oceane@example.com', '$2a$10$P5rgd08r1WzhACRjgP.ZlujPlox4cGu3WxC/MCIL3ZtZ4xdz05ZGu', 'Mariam'); -- password: coach123
+INSERT INTO users VALUES (DEFAULT, 2,'marie@example.com', '$2a$10$P5rgd08r1WzhACRjgP.ZlujPlox4cGu3WxC/MCIL3ZtZ4xdz05ZGu', 'Mariam'); -- password: coach123
 
 INSERT INTO users VALUES (DEFAULT, 3,'jamal@example.com', '$2a$10$Bmt9fki17Yqo90d/nPGu0.UR1vtyLhp6lKg3oaK6UyWSVn3tfLp3S', 'Jamal'); -- password: admin123
 
