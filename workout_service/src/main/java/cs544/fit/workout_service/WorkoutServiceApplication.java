a7f016d0d29c397d891f8863c6fdcf4117705ed2
magicmarie <natukunda162@gmail.com>
2025-05-21 16:39:26 -0500

    add more swagger documentation

--- workout_service/src/main/java/cs544/fit/workout_service/WorkoutServiceApplication.java
+++ workout_service/src/main/java/cs544/fit/workout_service/WorkoutServiceApplication.java
@@ -7,9 +7,11 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @OpenAPIDefinition(info = @Info(
 		title = "Workout Service API",
 		version = "1.0",
